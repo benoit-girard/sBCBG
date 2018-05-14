b10c50df003a623db619e3dc04223b5ee4919ad9
jealie <jean.lienard@gmail.com>
2018-05-14 11:17:52 +0900

    add stochastic_delays default value (no stochasticity)

--- baseParams.py
+++ baseParams.py
@@ -1,79 +1,92 @@
-# Simulation defaults
+# Base parameters and their meaning
 # these defaults can be overrided via a custom python parameter file or via the commandline (in this order: commandline arguments take precedence over customParams.py, which take precendence over the defaults defined here)
-params   =       {'durationH':           '08', # used by Sango
-                  'nbnodes':              '1', # used by K
-                  'nestSeed':              20, # nest seed (affects input poisson spike trains)
-                  'pythonSeed':            10, # python seed (affects connection map)
-                  'nbcpu':                  1,
-                  'whichTest':   'testFullBG',
-                  'nbCh':                   1,
-                  'LG14modelID':            9,
-                  'nbMSN':              2644.,
-                  'nbFSI':                53.,
-                  'nbSTN':                 8.,
-                  'nbGPe':                25.,
-                  'nbGPi':                14.,
-                  'nbCSN':              3000.,
-                  'nbPTN':               100.,
-                  'nbCMPf':                9.,
-                  'GMSN':                  1.,
-                  'GFSI':                  1.,
-                  'GSTN':                  1.,
-                  'GGPe':                  1.,
-                  'GGPi':                  1.,
-                  'IeMSN':                 0.,
-                  'IeFSI':                 0.,
-                  'IeSTN':                 0.,
-                  'IeGPe':                 0.,
-                  'IeGPi':                 0.,
-                  'inDegCSNMSN':      0.33333, # inDegrees are all set to 1/3 of maximal input population
-                  'inDegPTNMSN':      0.33333,
-                  'inDegCMPfMSN':     0.33333,
-                  'inDegFSIMSN':      0.33333,
-                  'inDegMSNMSN':      0.33333,
-                  'inDegSTNMSN':      0.33333,
-                  'inDegGPeMSN':      0.33333,
-                  'inDegCSNFSI':      0.33333,
-                  'inDegPTNFSI':      0.33333,
-                  'inDegSTNFSI':      0.33333,
-                  'inDegGPeFSI':      0.33333,
-                  'inDegCMPfFSI':     0.33333,
-                  'inDegFSIFSI':      0.33333,
-                  'inDegPTNSTN':      0.33333,
-                  'inDegCMPfSTN':     0.33333,
-                  'inDegGPeSTN':      0.33333,
-                  'inDegCMPfGPe':     0.33333,
-                  'inDegSTNGPe':      0.33333,
-                  'inDegMSNGPe':      0.33333,
-                  'inDegGPeGPe':      0.33333,
-                  'inDegMSNGPi':      0.33333,
-                  'inDegSTNGPi':      0.33333,
-                  'inDegGPeGPi':      0.33333,
-                  'inDegCMPfGPi':     0.33333,
-                  'cTypeCSNMSN':    'focused', # defining connection types for channel-based models (focused or diffuse) based on LG14
-                  'cTypePTNMSN':    'focused',
-                  'cTypeCMPfMSN':   'diffuse',
-                  'cTypeFSIMSN':    'diffuse',
-                  'cTypeMSNMSN':    'diffuse',
-                  'cTypeSTNMSN':    'diffuse',
-                  'cTypeGPeMSN':    'diffuse',
-                  'cTypeCSNFSI':    'focused',
-                  'cTypePTNFSI':    'focused',
-                  'cTypeSTNFSI':    'diffuse',
-                  'cTypeGPeFSI':    'diffuse',
-                  'cTypeCMPfFSI':   'diffuse',
-                  'cTypeFSIFSI':    'diffuse',
-                  'cTypePTNSTN':    'focused',
-                  'cTypeCMPfSTN':   'diffuse',
-                  'cTypeGPeSTN':    'focused',
-                  'cTypeCMPfGPe':   'diffuse',
-                  'cTypeSTNGPe':    'diffuse',
-                  'cTypeMSNGPe':    'focused',
-                  'cTypeGPeGPe':    'diffuse',
-                  'cTypeMSNGPi':    'focused',
-                  'cTypeSTNGPi':    'diffuse',
-                  'cTypeGPeGPi':    'diffuse', # LG14: no data available to decide; setting to diffuse improve selection properties
-                  'cTypeCMPfGPi':   'diffuse',
-                  'parrotCMPf' :      True,
-                  }
+
+####################################################################################
+# This file should not be modified! Use commandline arguments or custom param file #
+####################################################################################
+
+params = {
+'nbCh':                          1, # number of concurrent channels to simulate
+'LG14modelID':                   9, # LG 2014 parameterization used (default: model #9)
+'nbMSN':                     2644., # population size (default: 1/1000 of the BG)
+'nbFSI':                       53., # ^
+'nbSTN':                        8., # ^
+'nbGPe':                       25., # ^
+'nbGPi':                       14., # ^
+'nbCSN':                     3000., # ^
+'nbPTN':                      100., # ^
+'nbCMPf':                       9., # ^
+'GMSN':                         1., # gain on all synapses (default: no gain)
+'GFSI':                         1., # ^
+'GSTN':                         1., # ^
+'GGPe':                         1., # ^
+'GGPi':                         1., # ^
+'IeMSN':                        0., # tonic input currents (default: no input current)
+'IeFSI':                        0., # ^
+'IeSTN':                        0., # ^
+'IeGPe':                        0., # ^
+'IeGPi':                        0., # ^
+# There are 3 different format for setting the inDegreeXY (=number of different incoming neurons from X that target one neuron in Y)
+# - 'inDegreeAbs': specify the absolute number of different input neurons from X that target one neuron of Y --- be careful when using this setting, as the population size varies widly between the striatum and the downstream nuclei
+# - 'outDegreeAbs': specify the absolute number of contacts between an axon from X to each dendritic tree in Y
+# - 'outDegreeCons': specify the constrained number of contacts between an axon from X to each dendritic tree in Y as a fraction between 0 (minimal number of contacts to achieve required axonal bouton counts) and 1 (maximal number of contacts with respect to population numbers)
+'RedundancyType':   'outDegreeAbs', # by default all axons are hypothesized to target each dendritic tree at 3 different locations
+'redundancyCSNMSN':              3, # ^
+'redundancyPTNMSN':              3, # ^
+'redundancyCMPfMSN':             3, # ^
+'redundancyMSNMSN':              3, # ^
+'redundancyFSIMSN':              3, # ^
+'redundancySTNMSN':              3, # ^
+'redundancyGPeMSN':              3, # ^
+'redundancyCSNFSI':              3, # ^
+'redundancyPTNFSI':              3, # ^
+'redundancySTNFSI':              3, # ^
+'redundancyGPeFSI':              3, # ^
+'redundancyCMPfFSI':             3, # ^
+'redundancyFSIFSI':              3, # ^
+'redundancyPTNSTN':              3, # ^
+'redundancyCMPfSTN':             3, # ^
+'redundancyGPeSTN':              3, # ^
+'redundancyCMPfGPe':             3, # ^
+'redundancySTNGPe':              3, # ^
+'redundancyMSNGPe':              3, # ^
+'redundancyGPeGPe':              3, # ^
+'redundancyMSNGPi':              3, # ^
+'redundancySTNGPi':              3, # ^
+'redundancyGPeGPi':              3, # ^
+'redundancyCMPfGPi':             3, # ^
+'cTypeCSNMSN':           'focused', # defining connection types for channel-based models (focused or diffuse) based on LG14 - refer to this paper for justification
+'cTypePTNMSN':           'focused', # ^
+'cTypeCMPfMSN':          'diffuse', # ^
+'cTypeMSNMSN':           'diffuse', # ^
+'cTypeFSIMSN':           'diffuse', # ^
+'cTypeSTNMSN':           'diffuse', # ^
+'cTypeGPeMSN':           'diffuse', # ^
+'cTypeCSNFSI':           'focused', # ^
+'cTypePTNFSI':           'focused', # ^
+'cTypeCMPfFSI':          'diffuse', # ^
+'cTypeFSIFSI':           'diffuse', # ^
+'cTypeSTNFSI':           'diffuse', # ^
+'cTypeGPeFSI':           'diffuse', # ^
+'cTypePTNSTN':           'focused', # ^
+'cTypeCMPfSTN':          'diffuse', # ^
+'cTypeGPeSTN':           'focused', # ^
+'cTypeCMPfGPe':          'diffuse', # ^
+'cTypeMSNGPe':           'focused', # ^
+'cTypeSTNGPe':           'diffuse', # ^
+'cTypeGPeGPe':           'diffuse', # ^
+'cTypeCMPfGPi':          'diffuse', # ^
+'cTypeMSNGPi':           'focused', # ^
+'cTypeSTNGPi':           'diffuse', # ^
+'cTypeGPeGPi':           'diffuse', # LG14: no data available to decide; setting to diffuse improve selection properties
+'parrotCMPf' :                True, # Should the CMPf be simulated using parrot neurons?
+'stochastic_delays':          None, # If specified, gives the relative sd of a clipped Gaussian distribution for the delays
+# For convenience, a few simulator variables are also set here
+'whichTest':          'testFullBG', # task to be run (default: test the plausibility through deactivation simulations)
+'nestSeed':                     20, # nest seed (affects input poisson spike trains)
+'pythonSeed':                   10, # python seed (affects connection map)
+'nbcpu':                         1, # number of CPUs to be used by nest
+'durationH':                  '08', # max duration of a simulation, used by Sango cluster
+'nbnodes':                     '1', # number of nodes, used by K computer
+}
 
