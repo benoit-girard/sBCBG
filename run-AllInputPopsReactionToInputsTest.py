bf4067c9770cfd8cd8acbbc79c50bb0c1d082e59
Benoit Girard <benoit.girard@isir.upmc.fr>
2019-07-26 11:09:25 +0200

    Update run-AllInputPopsReactionToInputsTest.py
    detail

--- run-AllInputPopsReactionToInputsTest.py
+++ run-AllInputPopsReactionToInputsTest.py
@@ -4,7 +4,7 @@
 import os
 import time
 
-for modelID in [7,8]:
+for modelID in [10,11,12,13,14]:
 #for modelID in [3]:
   for p in ['CSN','PTN','CMPf']:
     commande = "python run.py --platform Local --LG14modelID "+str(modelID)+" --whichTest testReactionToInput --inputPop "+p+" --tag "+p+" --nbcpu -1 --nbCh 1 --gdf --custom hyperSphereParams/params"+str(modelID)+".py"
